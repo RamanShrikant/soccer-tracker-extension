c8a1ad39f9eeeab43454eafa47b743e462545380
RamanShrikant <ramanshrikant2@gmail.com>
2025-09-18 22:29:13 -0400

    Update DynamoDbConfig.java

--- soccer-backend/src/main/java/com/raman/soccer_backend/config/DynamoDbConfig.java
+++ soccer-backend/src/main/java/com/raman/soccer_backend/config/DynamoDbConfig.java
@@ -6,12 +6,12 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
-public class DynamoConfig {
+public class DynamoDbConfig {
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_2) // 👈 Ohio, match your AWS table region
+                .region(Region.US_EAST_2) // match your table’s region
                 .build();
     }
 }
